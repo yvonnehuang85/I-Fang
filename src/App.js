641e3e46edfed3f55c3f0727bf0922444fb0a38b
QPwVejjvW1 <yvonnehang90@gmail.com>
2021-03-08 17:21:45 -0600

    Intoduction

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import Introduction from './components/introduction'
+import Sidebar from './components/sidebar'
 
 class App extends Component {
   render() {
     return (
       <div id="colorlib-page">
         <div id="container-wrap">
-		<div id="colorlib-main">
-			<Introduction></Introduction>
-          	</div>
-      	</div>
+          <Sidebar></Sidebar>
+            <div id="colorlib-main">
+              <Introduction></Introduction>
+            </div>
+          </div>
       </div>
     );
   }
